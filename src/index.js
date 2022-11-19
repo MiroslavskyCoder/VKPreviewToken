12721a36b5d97e9bd16062872cae76437022f736
MiroslavskyCoder <alinowski2016@yandex.ru>
2022-11-19 23:08:10 +0300

    update: app index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 const electron = require("electron");
 const path = require("path");
-const minimist = require("minimist");
-
-console.log(minimist(process.argv, { "--": true }));
 
 if (typeof(electron) == "string") {
     process.exit(-1);
@@ -15,29 +12,58 @@ const {
 } = electron;
 
 function create() {
-    const win = new BrowserWindow({
-        width: 1000,
-        height: 600,
+    const loading = new BrowserWindow({ 
+        center: true,
+        backgroundColor: "#121212",
+        width: 512,
+        height: 512,
+        minHeight: 512,
+        minWidth: 512,
         webPreferences: {
-            preload: path.join(__dirname, "render.js"),
             contextIsolation: true
         },
-        backgroundColor: "#121212",
-        center: true,
-        titleBarStyle: 'hidden',
-        titleBarOverlay: { 
-            symbolColor: '#fff',
-            height: 40
-        },
-        transparent: true
-    }); 
-    
-    win.webContents.openDevTools();
-    win.loadFile(path.resolve(__dirname, "..", "deps/app/build/index.html"));
+        titleBarStyle: "hidden", 
+        resizable: false
+    });
 
-    ipcMain.handle("create-session", async (event, ...args) => {
-        console.log(args);
-        return {};
+    loading.loadFile(path.resolve(__dirname, "..", "assets/loading.html"));
+    loading.show(); 
+     
+    loading.on("ready-to-show", () => {
+        const main = new BrowserWindow({
+            width: 1000,
+            height: 600,
+            minWidth: 1000,
+            minHeight: 600,
+            webPreferences: {
+                preload: path.join(__dirname, "render.js"),
+                contextIsolation: true
+            },
+            backgroundColor: "#121212",
+            center: true,
+            titleBarStyle: 'hidden',
+            titleBarOverlay: { 
+                color: "rgba(18, 18, 18, 1)",
+                symbolColor: "rgba(255, 255, 255, 1)",
+                height: 40
+            }
+        });
+
+        main.loadFile(path.resolve(__dirname, "..", "deps/app/build/index.html"));
+
+        main.webContents.once("dom-ready", () => { 
+            loading.hide();
+            loading.close();
+
+            main.show(); 
+
+            ipcMain.handle("create-session", async (event, ...args) => {
+                console.log(args);
+                return {};
+            });
+        
+        });
+
     });
 }
 
